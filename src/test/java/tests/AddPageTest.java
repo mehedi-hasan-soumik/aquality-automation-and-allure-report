4e5213ad9f61aaaeab7659370c6b36591527fd1f
m.soumik <mehedi.soumik@365rm.com>
2026-08-04 05:48:01 +0600

    some changes v.1

--- src/test/java/tests/AddPageTest.java
+++ src/test/java/tests/AddPageTest.java
@@ -1,25 +1,37 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.AddPage;
 import pages.HomePage;
 
 import static io.qameta.allure.Allure.step;
 
-public class AddPageTest extends BaseTest{
-    HomePage homePage = new HomePage();
-    AddPage addPage = new AddPage();
+public class AddPageTest extends BaseTest {
+
+    private HomePage homePage;
+    private AddPage addPage;
+
+    @BeforeMethod
+    public void initPages() {
+        homePage = new HomePage();
+        addPage = new AddPage();
+    }
 
     @Test
-    public void TestAdd(){
+    public void testAdd() {
         step("Click Add and Remove Page");
         homePage.clickaddandRemovelink();
+
         step("Check Add and Remove Page is Displayed");
         Assert.assertTrue(addPage.isAddPageDisplayed(), "Add Page is not displayed");
+
         step("Click Add element");
         addPage.clickElementBtn();
+
         step("Click Remove Element");
         addPage.clickDeleteElement();
     }
 }
+
